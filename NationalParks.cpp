13cf623c9dedf4ba8f68e2d347ca31fdd4dc1114
Connor Bren <wildweegee101@gmail.com>
2024-08-02 14:01:37 -0600

    national parks

--- NationalParks.cpp
+++ NationalParks.cpp
@@ -4,6 +4,46 @@
 namespace NationalParks {
 	Types::DesignatePark DesignatePark;
 	Types::Edit Edit;
+	Types::FindPark FindPark;
+	Types::RestoreVisualState RestoreVisualState;
+
+	//void RemoveVisualState()
+
+	//bool DesignateParkInPlots(NationalParks* nationalParks, int playerId, int* baseCoord, const std::vector<int>& plotIds) {
+	//	DesignatePark(nationalParks, playerId, baseCoord);
+	//	ParkData* park = FindPark(nationalParks, baseCoord);
+	//
+	//	if (park == NULL) {
+	//		return false;
+	//	}
+	//
+	//
+	//
+	//	return true;
+	//}
+
+	//static int lDesignateParkInPlots(hks::lua_State* L) {
+	//	int playerId = hks::checkinteger(L, 1);
+	//	int coord[2]{
+	//		hks::checkinteger(L, 2),
+	//		hks::checkinteger(L, 3)
+	//	};
+	//
+	//	int arraySize = hks::objlen(L, 2);
+	//	std::vector<int> plotIds;
+	//	for (int i = 1; i <= arraySize; i++) {
+	//		hks::pushinteger(L, i);
+	//		hks::gettable(L, 2);
+	//
+	//		int plotId = hks::checkinteger(L, -1);
+	//
+	//		plotIds.push_back(plotId);
+	//		hks::pop(L, 1);
+	//	}
+	//
+	//	hks::pushboolean(L, DesignateParkInPlots(Edit(), playerId, coord, plotIds));
+	//	return 1;
+	//}
 
 	static int lDesignatePark(hks::lua_State* L) {
 		int playerId = hks::checkinteger(L, 1);
@@ -17,11 +57,38 @@ namespace NationalParks {
 		return 0;
 	}
 
+	static int lFindPark(hks::lua_State* L) {
+		int coord[2]{
+			hks::checkinteger(L, 1),
+			hks::checkinteger(L, 2)
+		};
+
+		ParkData* park = FindPark(Edit(), coord);
+		if (park != NULL) {
+			hks::pushlightuserdata(L, park);
+			return 1;
+		}
+		return 0;
+	}
+
+	static int lRestoreVisualState(hks::lua_State* L) {
+		if (!hks::isuserdata(L, 1)) {
+			hks::error(L, "Type mismatch: Expected userdata pointer to ParkData object!\n");
+			return 0;
+		}
+		auto park = (ParkData*)hks::touserdata(L, 1);
+
+		hks::pushboolean(L, RestoreVisualState(Edit(), park));
+		return 1;
+	}
+
 	int Register(hks::lua_State* L) {
 		std::cout << "Registering NationalParks!\n";
-		hks::createtable(L, 0, 1);
+		hks::createtable(L, 0, 3);
 
 		PushLuaMethod(L, lDesignatePark, "lDesignatePark", -2, "DesignatePark");
+		PushLuaMethod(L, lFindPark, "lFindPark", -2, "FindPark");
+		PushLuaMethod(L, lRestoreVisualState, "lRestoreVisualState", -2, "RestoreVisualState");
 
 		hks::pushlightuserdata(L, Edit());
 		hks::setfield(L, -2, "__instance");
@@ -35,5 +102,7 @@ namespace NationalParks {
 
 		DesignatePark = GetGameCoreGlobalAt<Types::DesignatePark>(DESIGNATE_PARK_OFFSET);
 		Edit = GetGameCoreGlobalAt<Types::Edit>(GET_NATIONAL_PARKS_OFFSET);
+		RestoreVisualState = GetGameCoreGlobalAt<Types::RestoreVisualState>(RESTORE_VISUAL_STATE_OFFSET);
+		FindPark = GetGameCoreGlobalAt<Types::FindPark>(FIND_PARK_OFFSET);
 	}
 }
--- NationalParks.h
+++ NationalParks.h
@@ -1,18 +1,41 @@
 #pragma once
 #include "HavokScript.h"
+#include "vector"
 
 namespace NationalParks {
 	typedef class NationalParks;
+	typedef struct ParkData;
 	namespace Types {
 		typedef NationalParks* (*__cdecl Edit)(void);
-		typedef void (*__thiscall DesignatePark)(NationalParks*, int playerId, int* plots);
+		typedef void (*__thiscall DesignatePark)(NationalParks*, int playerId, int* coord);
+		typedef bool (*__thiscall RestoreVisualState)(NationalParks*, ParkData*);
+		typedef ParkData* (*__thiscall FindPark)(NationalParks*, int* coord);
 	}
+
+// #pragma pack(push, 1)
+// 	struct ParkData {
+// 		int id;
+// 		int i1;
+// 		eastl::vector* plotIds;
+// 		void* p1;
+// 		char padding[0x18];
+// 		char* parkName;
+// 	};
+// #pragma pack(pop)
+
+	//extern bool DesignateParkInPlots(NationalParks*, int playerId, int* baseCoord, std::vector<int>& plotIds);
 
 	constexpr uintptr_t GET_NATIONAL_PARKS_OFFSET = 0x2363e0;
 	extern Types::Edit Edit;
 
 	constexpr uintptr_t DESIGNATE_PARK_OFFSET = 0x235830;
 	extern Types::DesignatePark DesignatePark;
+
+	constexpr uintptr_t RESTORE_VISUAL_STATE_OFFSET = 0x2389f0;
+	extern Types::RestoreVisualState RestoreVisualState;
+
+	constexpr uintptr_t FIND_PARK_OFFSET = 0x2362c0;
+	extern Types::FindPark FindPark;
 
 	extern int Register(hks::lua_State* L);
 	extern void Create();
